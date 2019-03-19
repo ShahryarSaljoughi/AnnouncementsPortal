b5ac4699be329133c3f23d01b920652c1e57c7fb
shahryar saljoughi <s.saljoughi@outlook.com>
2019-03-19 10:50:40 +0330

    Add project files.

---
+++ AnnouncementsPortal/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnnouncementsPortal
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
